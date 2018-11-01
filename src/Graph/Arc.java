1fabc29d337b4e86df42c334d35bea9edf4186b2
Hussain <almohri@cs.ku.edu.kw>
2018-11-01 04:08:58 +0000

    Initialized project

---
+++ src/Graph/Arc.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Graph;
+
+/**
+ *
+ * @author Hussain Almohri, Kansas State University
+ */
+public class Arc
+{
+    public int u;
+    public int v;
+    public float weight;
+}
---
+++ src/Graph/Initial.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Graph;
+
+/**
+ *
+ * @author almohri
+ */
+public class Initial
+{
+    String name;
+    double value;
+}
